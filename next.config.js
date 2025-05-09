a04fb7080d9d87e94715f859b2aed50bf21d778d
Aditya Kumar Tiwari <itisaddy7@gmail.com>
2025-05-09 09:51:05 +0530

    Version 0.4.1: Fix external package configuration and dependency versions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  transpilePackages: ['tsparticles', 'react-tsparticles'],
+  experimental: {
+    optimizePackageImports: ['framer-motion', 'react-intersection-observer']
+  }
 };
 
 module.exports = nextConfig; 
