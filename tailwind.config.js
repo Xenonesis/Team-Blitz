ba5b8a870f7fa47a1aa6d9c4f9344e36392bb8c7
Aditya Kumar Tiwari <itisaddy7@gmail.com>
2025-05-09 09:48:39 +0530

    Version 0.4: Update font configuration and fix styling issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+      },
       animation: {
         'float': 'float 6s ease-in-out infinite',
         'fade-in': 'fadeIn 0.5s ease-in forwards',
