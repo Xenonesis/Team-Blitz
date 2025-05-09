725f7f1bfa85885f34fdee0c292d26b2f671c91f
Aditya Kumar Tiwari <itisaddy7@gmail.com>
2025-05-09 09:52:44 +0530

    Version 0.4.1: Add sharp package and configure image optimization

--- next.config.js
+++ next.config.js
@@ -3,7 +3,16 @@ const nextConfig = {
   transpilePackages: ['tsparticles', 'react-tsparticles'],
   experimental: {
     optimizePackageImports: ['framer-motion', 'react-intersection-observer']
-  }
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig; 
